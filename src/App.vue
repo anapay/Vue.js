a4d515543c91b780a2590e847540a67006b31a06
anapay <anapay42@gmail.com>
2020-07-22 18:59:57 -0300

    changing css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
   <div id="app">
-    <h1>Guia da Bruxa Hacker!</h1>
+    <h1>Bruxa Hacker!</h1>
     <h3>Avada Kedavra</h3>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Seja bem vindo ao guia bruxo Vue.js App"/>
+    <Chat/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Chat from './components/Chat';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Chat
   }
+  
+  
+  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-style: italic;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+ 
+}
+body{
+   background-color: rgb(196, 240, 240);
+
 }
 </style>
