225c8c77c51b9e759321dd768f423a5721b7b198
anapay <anapay42@gmail.com>
2020-07-22 19:00:24 -0300

    component chat

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <p>Entre no Chat do discord</p>
+      <a href="https://discord.com/channels/735587522993848361/735587524046618746"><button><h3>Accio!</h3></button></a>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
