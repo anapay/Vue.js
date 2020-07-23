f9f7ab003b386271035438764da3e6cf84fa9c23
anapay <anapay42@gmail.com>
2020-07-22 21:38:57 -0300

    deploy

--- public/server.js
+++ public/server.js
@@ -7,4 +7,5 @@ app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res){
     res.sendfile(__dirname + "/dist/index.html")
 });
-app.listen("Server Started..");
+app.listen(port);
+console.log("Server started...");
