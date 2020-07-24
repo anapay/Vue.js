132f276cb8165f840115b9d1c5618d10d8c3b512
anapay <anapay42@gmail.com>
2020-07-24 19:30:21 -0300

    add componente

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,49 @@
     <h3>Avada Kedavra</h3>
     <h4>Nada de bruxa e muito menos de Hacker!</h4>
     <h5>Sou mais uma copiadora de código</h5>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Seja bem vindo ao guia bruxo do meu Web site"/>
-    <Chat/>
+    <!-- <img class="logo" alt="Vue logo" src="./assets/logo.png" /> -->
+    <HelloWorld msg="Seja bem vindo ao guia bruxo do meu Web site" />
+    <Chat />
+    <Descricao />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Chat from './components/Chat';
+import HelloWorld from "./components/HelloWorld.vue";
+import Chat from "./components/Chat";
+import Descricao from "./components/Descricao.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    Chat
+    Chat,
+    Descricao
   }
-  
-  
-  
-}
+};
 </script>
 
 <style>
 #app {
-  font-style: italic;
+  font-style: italic, 25px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
- 
 }
-body{
-   background-color: rgb(196, 240, 240);
-
+body {
+  background-color: rgb(196, 240, 240);
+}
+.logo {
+  background-color: rgb(10,23 55, 0.5);
+  max-width: 100%;
+  max-height: 100%;
+  position:relative;
+  top: 0%;
+  left: 0%;
+  width: auto;
+  height: auto;
+  
 }
 </style>
---
+++ src/components/Descricao.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <h5>Venha interagir!</h5>
+      <p>Entre no Chat do discord</p>
+      <a href="https://discord.com/channels/735587522993848361/735587524046618746"><button><h3>Accio!</h3></button></a>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Descricao',
+  
+}
+</script>
+
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+p {
+  list-style-type: none;
+  padding: 5px;
+}
+div {
+  
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+color: #2c3e50;
+}
+
+</style>
+
